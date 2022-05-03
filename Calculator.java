15a8053b2aa9353ed0e8eeb2fae3da9ba4c2f52d
diego pacori <dpacoria@unsa.edu.pe>
2022-05-03 13:30:38 -0500

    Resta realizada

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
 class Calculator {
   int add(int a, int b){ return 0; }
-  int sub(int a, int b){ return 0; }
+  int sub(int a, int b){ return a-b; }
   int mul(int a, int b){ return a * b; }
   int div(int a, int b){
     if(b==0){
