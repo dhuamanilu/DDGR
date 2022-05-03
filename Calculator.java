fe7c17c1271f0300d100df97315e40825816c6b3
Gustavo <gordono@unsa.edu.pe>
2022-05-03 13:19:11 -0500

    Implemented mul and mod functions

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,12 @@
 class Calculator {
   int add(int a, int b){ return 0; }
   int sub(int a, int b){ return 0; }
-  int mul(int a, int b){ return 0; }
+  int mul(int a, int b){ return a * b; }
   int div(int a, int b){
     if(b==0){
       return -1;
     }
     return a/b; 
   }
-  int mod(int a, int b){ return 0; }
+  int mod(int a, int b){ return a % b; }
 }
